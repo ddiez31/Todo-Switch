eb3dd45cbd615d9ae8932900a1483f50f140745f
David <david.diez31@free.fr>
2017-02-08 12:40:25 +0100

    save david

--- js/app.js
+++ js/app.js
@@ -1,12 +1,38 @@
 $(document).ready(function() {
+    // alert("Bienvenue");
+    console.log("Bienvenue");
+    $("ul").append("<li><input id='checkbox' type='checkbox'/> " + "ANALYSER LE CODE AVANT DE COMMENCER" + "</li>");
+
     $(".send").click(function() {
         var task = $("#tache").val();
-        $("ul").append("<li><input id='checkbox' type='checkbox'>" + task + "</li>")
+        if (task !== "") {
+            $("ul").append("<li><input id='checkbox' type='checkbox'/> " + task + "</li>");
+            localStorage.setItem("#tache", task);
+        };
+    });
 
-        $("#checkbox").click(function() {
-            if (this.checked) {
-                $($(this).parent()).css('text-decoration', 'line-through')
-            }
-        })
-    })
+
+    if ($("#checkbox").is(':checked')) {
+        $(this).parent().css('text-decoration', 'line-through');
+    }
+
+
+    // function removeFinished() {
+    //     var checkboxes = $("#checkbox").attr();
+    //     for (var i = 0; i < checkboxes.length; i++) {
+    //         if (checkboxes[i].checked) {
+    //             //checkboxes[i].parentElement.remove();
+    //             checkboxes[i].parentElement.style.display = "none";
+    //         }
+    //     }
+    // }
+
+
+
+    $('#remove').click(function() {
+        $().css("display", "none");
+    });
+
+
+
 });
